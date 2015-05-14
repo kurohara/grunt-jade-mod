38e5ec7860a0ab895cc43720142b11561f0e809b
Hiroyoshi Kurohara <kurohara@yk.rim.or.jp>
2015-05-14 15:27:46 +0900

    changed the modifier protocol

--- tasks/jade.js
+++ tasks/jade.js
@@ -8,84 +8,78 @@
 
 'use strict';
 
+var restorefuncs = [];
+
 module.exports = function(grunt) {
 
-  var sys = require('sys');
-  var module = require('../node_modules/grunt-contrib-jade/tasks/jade');
-  module(grunt);
+	var sys = require('sys');
+	var jadetask = require('../node_modules/grunt-contrib-jade/tasks/jade');
+	jadetask(grunt);
 
-  if (grunt.task.exists('jade')) {
-    grunt.task.renameTask('jade', 'jade_orig');
-  }
+	if (grunt.task.exists('jade')) {
+		grunt.task.renameTask('jade', 'jade_orig');
+	}
 
-  /**
-   * Function to restore original functions.
-   *
-   */
-  var clearJadeModifiers = function (obj) {
-    if (obj) {
-      // write back original functions.
-      [ "Parser", "Compiler", "Lexer" ].forEach(function(component) {
-        for (var key in obj[component].prototype) {
-          if (key.lastIndexOf('ex_', 0) === 0) {
-            obj[component].prototype[key.substring(3)] = obj[component].prototype[key];
-          }
-        }
-      });
-    }
-  };
+	// register postprocess task.
+	grunt.registerMultiTask('jade_post_orig', 'Postprocess to restore original settings.', function() {
+		// nothing todo now.
+	});
 
-  // register postprocess task.
-  grunt.registerMultiTask('jade_post_orig', 'Postprocess to restore original settings.', function() {
-    // nothing todo now.
-  });
+	function callRestore() {
+		restorefuncs.forEach(function(f) {
+			f();
+		});
+	}
+	// register overwrapping 'jade' task.
+	grunt.registerMultiTask('jade', 'Compile jade templates, with modifiers.', function() {
 
-  // register overwrapping 'jade' task.
-  grunt.registerMultiTask('jade', 'Compile jade templates, with modifiers.', function() {
+		//
+		// 1. Replace 'require' function.
+		//
+		var yor = require('yorequire');
+		yor.setCB(function(name, o_require, data) {
+			var obj = o_require(name);
 
-    //
-    // 1. Replace 'require' function.
-    //
-    var Module = require('module');
-    if (! Module.prototype.orig_require) {
-      Module.prototype.orig_require = Module.prototype.require;
-    }
+			if (name === 'jade') {
+				var jade = obj;
+				callRestore();
+				restorefuncs = [];
+				var modifiers = data['modifiers'];
+				if (modifiers) {
+					for (var index in modifiers) {
+						var modobj = o_require(modifiers[index]);
+						//
+						// modifiers can, 
+						// 1. override prototype functions of jade.(Lexer|Parser|Compiler).
+						// or
+						// 2. make jade use of your own Parser|Compiler by adding class to options.parser, options.compiler.
+						//         (this is not tested yet, even about its feasibility)
+						//
+						modobj.init(jade);
+						restorefuncs.push(modobj.restorefunc());
+					}
+				}
+			}
+	
+			return obj;
+		}, this.options({}));
+		yor.enable(true);
 
-    Module.prototype.require = function (name) {
-      var obj = this.orig_require(name);
+		//
+		// 2. Enqueue original 'jade' task and postprocess task.
+		//
+		var taskq = [];
 
-      if (name === 'jade') {
-        clearJadeModifiers(obj);
-        var modifiers = this.require_options['modifiers'];
-        if (modifiers) {
-          for (var index in modifiers) {
-            var modobj = this.orig_require(modifiers[index]);
-            modobj(obj);
-          }
-        }
-      }
+		// prepare to call 'original jade' task.
+		grunt.config.set('jade_orig', grunt.config.getRaw('jade'));
+		taskq.push('jade_orig' + ':' + this.target);
 
-      return obj;
-    };
-    Module.prototype.require_options = this.options({});
+		// prepare to call postproc task.
+		grunt.config.set('jade_post_orig', { default: {} } );
+		taskq.push('jade_post_orig');
 
-
-    //
-    // 2. Enqueue original 'jade' task and postprocess task.
-    //
-    var taskq = [];
-
-    // prepare to call 'original jade' task.
-    grunt.config.set('jade_orig', grunt.config.getRaw('jade'));
-    taskq.push('jade_orig' + ':' + this.target);
-
-    // prepare to call postproc task.
-    grunt.config.set('jade_post_orig', { default: {} } );
-    taskq.push('jade_post_orig');
-
-    // enqueue task.
-    grunt.task.run(taskq);
-
-  });
-
+		// enqueue task.
+		grunt.task.run(taskq);
+	}); // end of 'jade' task.
 };
+
--- test/modifiers/jade4jsp/index.js
+++ test/modifiers/jade4jsp/index.js
@@ -3,9 +3,6 @@
  * part of jade4jsp, the sample modifier for jade.
  * Written by "Hiroyoshi Kurohara<kurohara@yk.rim.or.jp>".
  *
- * This source code is based on 'jade'(http://jade-lang.com) and 'jade-php'.
- * 'jade-php' is written by "Vinicius Wrubleski <vinicius.wrubleski@gmail.com>".
  */
-module.exports = function (jade) {
-    require('./lib/compiler')(jade);
-};
+module.exports = require('./lib/compiler');
+
