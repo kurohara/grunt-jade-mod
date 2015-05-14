d6573b472c4b6fcf124ac2ef0c1bc555be9f5080
Hiroyoshi Kurohara <kurohara@yk.rim.or.jp>
2015-05-14 15:56:13 +0900

    rewrote test/modifiers/jade4jsp

--- test/modifiers/jade4jsp/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * index.js
- * part of jade4jsp, the sample modifier for jade.
- * Written by "Hiroyoshi Kurohara<kurohara@yk.rim.or.jp>".
- *
- */
-module.exports = require('./lib/compiler');
-
--- test/modifiers/jade4jsp/modifier.js
+++ test/modifiers/jade4jsp/modifier.js
@@ -1,10 +1,8 @@
 /**
- * compiler.js
+ * modifier.js
  * part of jade4jsp, the sample modifier for jade.
  * Written by "Hiroyoshi Kurohara<kurohara@yk.rim.or.jp>".
  *
- * This source code is based on 'jade'(http://jade-lang.com) and 'jade-php'.
- * 'jade-php' is written by "Vinicius Wrubleski <vinicius.wrubleski@gmail.com>".
  */
 
 var Modifier = function Modifier() {
